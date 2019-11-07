d0e4ff8cf5ef2dff9af9a7e3260203ac95c9a505
ျပည့္စံုဟိန္း <pyaesonehein1141991@gmail.com>
2019-11-07 10:09:17 +0630

    First upload

---
+++ src/test/java/com/ace/demoapi/CiCdAppliedToSpringBootJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace.demoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdAppliedToSpringBootJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
